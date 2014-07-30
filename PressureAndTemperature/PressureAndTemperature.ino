7f7eefc3a49f146f825f3499e9132150dfd3bf61
Meha-Agrawal <agra.meha@hotmail.com>
2014-07-30 15:43:40 -0400

    Pressure and Temperature
    Pressure and Temperature sensors are both transmitting their states to
    the serial and they aren't affecting each other, but are also changing
    when we change it. So they are working separately on the same circuit
    board and on the same code. We combined the pressure sensor code and the
    temperature sensor code.

---
+++ PressureAndTemperature/PressureAndTemperature.ino
@@ -0,0 +1,85 @@
+/* FSR testing sketch. 
+ 
+Connect one end of FSR to power, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground 
+ 
+For more information see www.ladyada.net/learn/sensors/fsr.html */
+ 
+int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
+int fsrReading;     // the analog reading from the FSR resistor divider
+int fsrVoltage;     // the analog reading converted to voltage
+unsigned long fsrResistance;  // The voltage converted to resistance, can be very big so make "long"
+unsigned long fsrConductance; 
+long fsrForce;       // Finally, the resistance converted to force
+
+int sensorPin = 1; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+ 
+void setup(void) {
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+
+}
+ 
+void loop(void) {
+  fsrReading = analogRead(fsrPin);  
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+ 
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  Serial.print("Voltage reading in mV = ");
+  Serial.println(fsrVoltage);  
+ 
+  if (fsrVoltage == 0) {
+    Serial.println("No pressure");  
+  } else {
+    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
+    // so FSR = ((Vcc - V) * R) / V        yay math!
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    Serial.print("FSR resistance in ohms = ");
+    Serial.println(fsrResistance);
+ 
+    fsrConductance = 1000000;           // we measure in micromhos so 
+    fsrConductance /= fsrResistance;
+    Serial.print("Conductance in microMhos: ");
+    Serial.println(fsrConductance);
+ 
+    // Use the two FSR guide graphs to approximate the force
+    if (fsrConductance <= 1000) {
+      fsrForce = fsrConductance / 80;
+      Serial.print("Force in Newtons: ");
+      Serial.println(fsrForce);      
+    } else {
+      fsrForce = fsrConductance - 1000;
+      fsrForce /= 30;
+      Serial.print("Force in Newtons: ");
+      Serial.println(fsrForce);            
+    }
+  }
+  Serial.println("--------------------");
+  delay(1000);
+  
+  //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+ 
+ delay(1000);                                     //waiting a second
+}
