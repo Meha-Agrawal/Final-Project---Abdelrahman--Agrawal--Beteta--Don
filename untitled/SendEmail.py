fa351cd270f289bd52e5813d446725f8255a9fe9
Meha-Agrawal <agra.meha@hotmail.com>
2014-07-31 13:45:53 -0400

    The email code on Python
    This is the code that takes the commands from the serial from the
    arduino and actually performs the act of sending the email.

---
+++ untitled/SendEmail.py
@@ -0,0 +1,32 @@
+import time
+import serial
+import smtplib
+
+TO = 'salma.zakaria@gmail.com'
+GMAIL_USER = 'tempalertgwc@gmail.com'
+GMAIL_PASS = 'GWCROCKS'
+SUBJECT = 'It works'
+TEXT = 'We rule :)'
+ser = serial.Serial('COM3', 9600)
+
+def send_email():
+    print("Sending Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT + '\n'
+    print header
+    msg = header + '\n' + TEXT + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+
+while True:
+    print("hi")
+    message = ser.readline()
+    print(message)
+    if message[0] == 'M' :
+        send_email()
+    time.sleep(0.5)
